6ce6875e3b91e092d4e4b9a08f89515a29bdda4a
roberton <rnerici@gmail.com>
2023-12-22 18:26:30 +0000

    feat: completes Hashtag Generator kata

--- javascript/HashtagGenerator/code.js
+++ javascript/HashtagGenerator/code.js
@@ -1,4 +1,15 @@
 export default function generateHashtag (str) {
-  return false;
-}
+  const words = str.split(' ').map(word => word.trim()).filter(word => word.length > 0)
+  if (!words.length) {
+    return false
+  }
 
+  const camelCaseWords = words.map(word => word[0].toUpperCase() + word.slice(1))
+  const hashtag = `#${camelCaseWords.join('')}`
+
+  if (hashtag.length > 140) {
+    return false
+  }
+
+  return hashtag
+}
--- javascript/HashtagGenerator/test.js
+++ javascript/HashtagGenerator/test.js
@@ -1,13 +1,26 @@
-import { test, describe, it } from "node:test";
-import assert from "node:assert/strict";
-import generateHashtag from "./code.js";
+import { test, describe, it } from "node:test"
+import assert from "node:assert/strict"
+import generateHashtag from "./code.js"
 
 describe("codewars tests", () => {
-  it("Sample tests", () => {
-    assert.strictEqual(generateHashtag(""), false, "Expected an empty string to return false")
+  it("Expected an empty string to return false", () => {
+    assert.strictEqual(generateHashtag(""), false)
+  })
+  it("Still an empty string", () => {
+    assert.strictEqual(generateHashtag(" ".repeat(200)), false)
+  })
+  it("Expected a Hashtag (#) at the beginning.", () => {
+    assert.strictEqual(generateHashtag("Do We have A Hashtag"), "#DoWeHaveAHashtag")
+  })
+  it("Should handle a single word.", () => {
+    assert.strictEqual(generateHashtag("Codewars"), "#Codewars")
+  })
+  it("Other sample tests", () => {
+    assert.strictEqual(generateHashtag("Codewars Is Nice"), "#CodewarsIsNice", "Should remove spaces.")
+    assert.strictEqual(generateHashtag("Codewars is nice"), "#CodewarsIsNice", "Should capitalize first letters of words.")
+    assert.strictEqual(generateHashtag("code" + " ".repeat(140) + "wars"), "#CodeWars")
+    assert.strictEqual(generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"), false, "Should return false if the final word is longer than 140 chars.")
+    assert.strictEqual(generateHashtag("a".repeat(139)), "#A" + "a".repeat(138), "Should work")
+    assert.strictEqual(generateHashtag("a".repeat(140)), false, "Too long")
   })
 })
-
-test("will pass", () => {
-  assert.ok("hello world");
-});
