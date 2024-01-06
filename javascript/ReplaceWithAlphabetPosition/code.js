01fbef7b0ac0f1d864521d1b6c012d3dcad25d59
roberton <rnerici@gmail.com>
2024-01-06 13:37:06 +0000

    feat: implements ReplaceWithAlphabetPositionkata

--- javascript/ReplaceWithAlphabetPosition/code.js
+++ javascript/ReplaceWithAlphabetPosition/code.js
@@ -1,7 +1,18 @@
 function alphabetPosition(text) {
-  return text
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  const positions = text
+    .toLowerCase()
+    .split('')
+    .filter(letter => alphabet.includes(letter))
+    .map(letter => letterPosition(letter))
+
+  return positions.join(' ')
 }
 
+// assumes lower case
+const letterPosition = (letter) => letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1
+
 export {
-  alphabetPosition
+  alphabetPosition,
+  letterPosition
 }
--- javascript/ReplaceWithAlphabetPosition/test.js
+++ javascript/ReplaceWithAlphabetPosition/test.js
@@ -1,6 +1,12 @@
 import { describe, it } from "node:test"
 import assert from "node:assert/strict"
-import { alphabetPosition } from "./code.js"
+import { alphabetPosition, letterPosition } from "./code.js"
+
+describe('letterPosition', () => {
+  it('a -> 1', () => {
+    assert.strictEqual(letterPosition('a'), 1)
+  })
+})
 
 describe('alphabetPosition', () => {
   it("The sunset sets at twelve o'clock. -> '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'", () => {
