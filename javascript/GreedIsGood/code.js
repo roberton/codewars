b5ce1f8191a7cc03a7fab87a502f19a6d409cc08
roberton <rnerici@gmail.com>
2024-01-13 15:57:16 +0000

    chore: slightly improves GreedIsGood solution

--- javascript/GreedIsGood/code.js
+++ javascript/GreedIsGood/code.js
@@ -1,24 +1,32 @@
 function score(dice) {
   let totalScore = 0
+  let spareDice = dice
+  const tripletScores = [
+    {die: 1, score: 1000},
+    {die: 6, score: 600},
+    {die: 5, score: 500},
+    {die: 4, score: 400},
+    {die: 3, score: 300},
+    {die: 2, score: 200},
+  ]
+  const singleScores = [
+    {die: 1, score: 100},
+    {die: 1, score: 100},
+    {die: 5, score: 50},
+    {die: 5, score: 50},
+  ]
 
-  const [score, spareDice] = checkForTriplet(dice, 1)
-  totalScore += score
+  tripletScores.forEach(scoreInfo => {
+    let found
+    [found, spareDice] = checkForTriplet(spareDice, scoreInfo.die)
+    if (found) { totalScore += scoreInfo.score }
+  })
 
-  const [score2, spareDice2] = checkForTriplet(spareDice, 6)
-  totalScore += score2
-  const [score3, spareDice3] = checkForTriplet(spareDice2, 5)
-  totalScore += score3
-  const [score4, spareDice4] = checkForTriplet(spareDice3, 4)
-  totalScore += score4
-  const [score5, spareDice5] = checkForTriplet(spareDice4, 3)
-  totalScore += score5
-  const [score6, spareDice6] = checkForTriplet(spareDice5, 2)
-  totalScore += score6
-
-  const [score7, spareDice7] = checkForSingle(spareDice6, 1)
-  totalScore += score7
-  const [score8, spareDice8] = checkForSingle(spareDice7, 5)
-  totalScore += score8
+  singleScores.forEach(scoreInfo => {
+    let found
+    [found, spareDice] = checkForSingle(spareDice, scoreInfo.die)
+    if (found) { totalScore += scoreInfo.score }
+  })
 
   return totalScore
 }
@@ -28,24 +36,23 @@ function checkForTriplet(dice, number) {
   const spareDice = dice.filter(d => d !== number)
 
   if (diceWithNumber.length >= 3) {
-    const score = (number === 1) ? 1000 : number * 100
-
     // Hack!
     if (diceWithNumber.length > 3) spareDice.push(number)
     if (diceWithNumber.length > 4) spareDice.push(number)
 
-    return [score, spareDice]
+    return [true, spareDice]
   }
-  return [0, dice]
+  return [false, dice]
 }
 
 function checkForSingle(dice, number) {
   const spareDice = dice.filter(d => d !== number)
-
-  const scoreForNumber = number === 1 ? 100 : 50
   const numDiceFound = dice.length - spareDice.length
 
-  return [scoreForNumber * numDiceFound, spareDice]
+  // Hack!
+  if (numDiceFound > 1) spareDice.push(number)
+
+  return [numDiceFound !== 0, spareDice]
 }
 
 export {
